577f5659378c051ec762549051c508590358f73e
Hugarra <75989236+Hugarra@users.noreply.github.com>
2021-05-10 21:28:38 -0300

    Contact charging icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com'
 
 
 function Contact(props) {
 
+    const [enviandoEmail, setEnviandoEmail] = useState(false);
+    const [alert, setAlert] = useState('');
+
     function sendEmail(e) {
         e.preventDefault();
+        setEnviandoEmail(true);
 
         emailjs.sendForm('service_nkdx284', 'template_zy3ugkp', e.target, 'user_JPwNN0pkNzOXzxoio7L8c')
             .then((result) => {
                 console.log(result.text);
+                setAlert(result.text);
+                setEnviandoEmail(false);
             }, (error) => {
                 console.log(error.text);
-            }); 
+                setAlert(error.text);
+                setEnviandoEmail(false);
+            });
     } 
 
     return (
@@ -24,9 +32,25 @@ function Contact(props) {
                     <input className="mb-3 form-control" type="email" placeholder="E-mail" name="from_email" />
                     <textarea className="mb-3 form-control" placeholder="Message" name="message"/>
 
+                    {
+                        enviandoEmail &&
+                            <div classname="container">
+                                <div className="mb-3 spinner-border m-auto" role="status">
+                                    <span className="visually-hidden">Loading...</span>
+                                </div>
+                            </div>
+                    }
+                    {
+                        alert !== '' &&
+                            <div classname="container">
+                                <div className="mt-3 alert alert-success fade show border">
+                                    <p>{alert}</p>
+                                </div>
+                            </div>
+                    }
+
                     <button className="btn btn-primary" type="submit">Enviar!</button>
                 </form>
-
             </div>
         </div>
     );
